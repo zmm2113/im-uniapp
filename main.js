ce077667b0b4112d3a8e2675dcc149f7b5923a25
zmm2113 <zmm2113@qq.com>
2022-08-09 18:17:41 +0800

    socket

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ import http from '@/common/request'
 import store from './store'
 import md5 from "@/common/md5.js";
 import publicFc from "@/common/publicFc.js";
+// #ifdef H5
+import socketTask from "@/common/socketTask.js";
+// #endif
 import zmmFormCheck from './common/zmmFormCheck.js';
 import pinyin from './common/pinyin.js';
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+
+
+
 
 Vue.config.productionTip = false
 Vue.prototype.$http = http
@@ -15,6 +21,9 @@ Vue.prototype.$fc = publicFc;
 Vue.prototype.$md5 = md5
 Vue.prototype.$zmmFormCheck = zmmFormCheck;
 Vue.prototype.$pinyin = pinyin;
+// #ifdef H5
+Vue.prototype.$socketTask = socketTask;
+// #endif
 App.mpType = 'app'
 
 const app = new Vue({
@@ -34,6 +43,9 @@ export function createApp() {
   app.config.globalProperties.$md5 = md5
   app.config.globalProperties.$zmmFormCheck = zmmFormCheck
   app.config.globalProperties.$pinyin = pinyin
+  // #ifdef H5
+  app.config.globalProperties.$socketTask = socketTask
+  // #endif
   app.use(store);
   return {
     app
